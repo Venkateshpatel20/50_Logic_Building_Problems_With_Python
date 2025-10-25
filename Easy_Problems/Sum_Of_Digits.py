667eeb2580673da344dd4760f57607656d6a0f8c
Venkateshpatel20 <venkateshpatel18@gmail.com>
2025-10-26 00:02:46 +0530

    Updated logic

--- Easy_Problems/Sum_Of_Digits.py
+++ Easy_Problems/Sum_Of_Digits.py
@@ -8,3 +8,28 @@ if __name__ == "__main__":
     print(sum_of_digits(9999999))
     print(sum_of_digits(9999997))
     print(sum_of_digits(9999996))
+
+
+# *** Digits Extraction Method ***
+
+def sumOfDigits(n):
+    sum = 0
+    while n != 0:
+
+        last = n % 10
+        sum += last
+        n //= 10
+    return sum
+
+if __name__ == "__main__":
+	n = 12345
+	print(sumOfDigits(n))
+     
+
+
+# My favourite String method 
+number = "29783569283"
+sum:int=0
+for ch in number:
+     sum += int(ch)
+print(sum)
