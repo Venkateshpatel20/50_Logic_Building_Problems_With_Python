5a0feb01e96160ef8419569786c300ebac979e1b
Venkateshpatel20 <venkateshpatel18@gmail.com>
2025-10-25 23:44:10 +0530

    Add dice problem

---
+++ Basic_Problems/dice_problem.py
@@ -0,0 +1,11 @@
+def oppositeFaceOfDice(n):
+    
+    return 7 - n
+
+if __name__ == "__main__":
+    print(oppositeFaceOfDice(1))
+    print(oppositeFaceOfDice(2))
+    print(oppositeFaceOfDice(3))
+    print(oppositeFaceOfDice(4))
+    print(oppositeFaceOfDice(5))
+    print(oppositeFaceOfDice(6))
