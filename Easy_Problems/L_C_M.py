6176839c0565e1d198107381231afe84391ba72f
Venkateshpatel20 <venkateshpatel18@gmail.com>
2025-11-17 00:38:18 +0530

    Optimize performance

--- Easy_Problems/L_C_M.py
+++ Easy_Problems/L_C_M.py
@@ -1,7 +1,23 @@
 #Finding the LCM < least common faactor > of two numbers 
-#Brute force approach
 
 class numberSeries:
+    # Optimal approach
+    # function for gcd
+    # Time Complexity: O(log(min(a,b))
+    # Auxiliary Space: O(log(min(a,b))
+    def gcd(self,a, b):
+        return a if b == 0 else self.gcd(b, a % b)
+
+    def lcm(self,a, b):
+        return (a // self.gcd(a, b)) * b
+
+
+    
+
+    #Brute force approach
+    # Time Complexity: O(min(a,b))
+    # Auxiliary Space: O(1)
+
     def find_lcm(self,a,b):
         small_num = min(a,b)
         large_num = max(a,b)
@@ -16,5 +32,10 @@ if __name__ == "__main__":
     a , b = 12 , 4
     c , d = 11 , 7
     obj = numberSeries()
+    print(obj.lcm(a,b))
+    print(obj.lcm(c,d))
     print(obj.find_lcm(a,b))
     print(obj.find_lcm(c,d))
+
+
+
